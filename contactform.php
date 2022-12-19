08bc1ebd771ba96c5c5f212fed20db2691c3d4f4
Szymon <szymonwiderka@wp.pl>
2022-12-19 18:39:06 +0100

    s

---
+++ contactform.php
@@ -0,0 +1,34 @@
+<?php 
+if (isset($_POST['submit'])){
+
+$name = $_POST['name'];
+$mailFrom = $_POST['email'];
+$phone = $_POST['phone'];
+$message = $_POST['message'];
+
+$mailTo = "szymonwiderka66@gmail.com";
+$headers = "From: ".$mailFrom;
+$txt = "You have received an e-mail from".$name.".\n\n".$message;
+
+
+
+mail($mailTo, $subject, $txt, $headers);
+header("Location: index.php?mailsend")
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
