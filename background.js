ea624fdbe5f46bb98ad68731820b0caeb97b7c0b
Szymon <szymonwiderka@wp.pl>
2023-12-24 15:03:29 +0100

    Update background.js
    df

--- background.js
+++ background.js
@@ -1,12 +1,12 @@
 function lines() {
-    let sizeW = Math.random() * 12;
-    let duration = Math.random() * 3;
+    let sizeW = Math.random() * 15;
+    let duration = Math.random() * 5;
     let e = document.createElement('div');
     e.setAttribute('class', 'circle');
     document.body.appendChild(e);
     e.style.width = 2+sizeW+'px';
     e.style.left = Math.random() * + innerWidth + 'px';
-    e.style.animationDuration = 2 + duration+'s';
+    e.style.animationDuration = 3 + duration+'s';
 
 
 
